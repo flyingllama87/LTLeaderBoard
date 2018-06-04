9544ceb0a340f2c7983be4b83eb745895db629e9
flyingllama87 <>
2018-06-04 11:49:56 +1000

    Use argparse to allow specific score to be added via the commandline.  Add test case for invalid data to API

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ from traceback import print_exc
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--AddScore", help="Add Score to Leaderboard only", action="store_true")
-parser.add_argument("Name", type=str, help="Name of player whose score you wish to add")
-parser.add_argument("Score", type=int, help="Score of player whose score you wish to add")
+parser.add_argument("--AddScore", nargs=2, metavar="NAME SCORE", help="Add Score to Leaderboard only", dest="score")
+# parser.add_argument("Name", type=str, help="Name of player whose score you wish to add")
+# parser.add_argument("Score", type=int, help="Score of player whose score you wish to add")
 
 args = parser.parse_args()
 
@@ -49,9 +49,9 @@ def test_hello():
     finally:
         print('Response:', response, '\n')
 
-def test_AddScore():
+def test_AddScore(name = 'Morgan', score = 5):
     print("Performing 'AddScore' API call test:")
-    response = server.app.AddScore('Morgan', 5)
+    response = server.app.AddScore(name, score)
     try:
         assert "Score added" in response['result']
         assert "error" not in response
@@ -64,6 +64,21 @@ def test_AddScore():
     finally:
         print('Response:', response, '\n')
     
+
+def test_AddScoreInvalidData(name = 'Player', score = 'PlayerScore'):
+    print("Performing 'AddScore' API call test with invalid data:")
+    response = server.app.AddScore(name, score)
+    try:
+        assert "Score added" not in response['result']
+        assert "error" in response
+        print(f'{Fore.GREEN}AddScore test passed{Style.RESET_ALL}')
+    except:
+        print(f'{Fore.RED}AddScore test failed{Style.RESET_ALL}')
+        global failed_tests
+        failed_tests = True
+        print_exc()
+    finally:
+        print('Response:', response, '\n')
 
 def test_GetScores():
     print("Performing 'Getscores' API call test:")
@@ -87,12 +102,13 @@ def test_GetScores():
 
 
 if __name__ == "__main__":
-    if args.AddScore:
-        test_AddScore(args.name, args.score)
+    if args.score:
+        test_AddScore(args.score[0], args.score[1])
     else:
         test_index()
         test_hello()
         test_AddScore()
+        test_AddScoreInvalidData()
         test_GetScores()
         if failed_tests == False:
             print(f'{Fore.GREEN} ALL TESTS PASSED!{Style.RESET_ALL}')

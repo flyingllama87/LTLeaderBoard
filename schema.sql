6fc015ccc752b91a1d7acf7d1107bc179204d017
Morgan Robertson <>
2018-07-07 11:03:25 +1000

    Add check for negative scores in DB schema

--- schema.sql
+++ schema.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS scores;
 CREATE TABLE scores (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT CHECK (name is null or length(name) > 0),
-    score INTEGER NOT NULL CHECK (typeof(score) = 'integer')
+    score INTEGER NOT NULL CHECK (typeof(score) = 'integer' and score > 0)
 );

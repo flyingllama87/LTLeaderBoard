6bb93b4486e7e171146f3b3f2b91d7771e38714d
Morgan Robertson <>
2018-07-21 04:19:45 +1000

    Include support for difficulty in flask app and DB schema

--- LTLeaderBoard/__init__.py
+++ LTLeaderBoard/__init__.py
@@ -18,13 +18,13 @@ def index():
     return u'Welcome to flask JSON-RPC!'
 
 @jsonrpc.method('app.AddScore')
-def AddScore(name="Player", score=1):
+def AddScore(name="Player", score=1, difficulty="EASY"):
     try:
         score = abs(int(score))
         db_connection = db.get_db()
-        previous_score = db_connection.execute('SELECT * FROM scores WHERE name = ?', (name, )).fetchone()
+        previous_score = db_connection.execute('SELECT * FROM scores WHERE name = ? AND difficulty = ?', (name, difficulty)).fetchone()
         if previous_score and previous_score['score'] < score:
-            db_connection.execute('UPDATE scores SET name = ?, score = ? WHERE name = ?', (name, score, name))
+            db_connection.execute('UPDATE scores SET name = ?, score = ? WHERE name = ? AND difficulty = ?', (name, score, name, difficulty))
             db_connection.commit()
             print("Score updated by client.")
             return "Score updated"
@@ -32,8 +32,8 @@ def AddScore(name="Player", score=1):
             print("Score rejected by server as existing score already exists for player with a higher or equal value")
             return "Score not updated. Submitted score equal to or lower than existing value."
         else:
-            db_connection.execute('INSERT INTO scores (name, score) VALUES (?, ?)',
-            (name, score))
+            db_connection.execute('INSERT INTO scores (name, score, difficulty) VALUES (?, ?, ?)',
+            (name, score, difficulty))
             db_connection.commit()
             print("Score added by client.")
             return "Score added"
@@ -53,6 +53,7 @@ def GetScores():
             score = {}
             score['name'] = row['name']
             score['score'] = row['score']
+            score['difficulty'] = row['difficulty']
             scores.append(score)
         print("Received request to get scores")
         return json.dumps(scores)
--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS scores;
 CREATE TABLE scores (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT CHECK (name is null or length(name) > 0),
-    score INTEGER NOT NULL CHECK (typeof(score) = 'integer' and score > 0)
+    score INTEGER NOT NULL CHECK (typeof(score) = 'integer' and score > 0),
+    difficulty TEXT CHECK (difficulty == "EASY" or difficulty == "MEDIUM" or difficulty == "HARD")
 );
